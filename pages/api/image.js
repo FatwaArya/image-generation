6a81054777202c2a19a438c260d7d1cbf6b7dc34
Fatwa <fatwaaryasatyaakbar@gmail.com>
2022-11-06 20:31:44 +0700

    send 3 images

--- pages/api/image.js
+++ pages/api/image.js
@@ -12,11 +12,11 @@ export default async function handler(req, res) {
     const { prompt } = req.body;
     const response = await openai.createImage({
       prompt,
-      n: 1,
+      n: 3,
       size: "256x256",
     });
-    const image = response.data.data[0];
-    res.status(200).json({ image });
+
+    res.status(200).json({ image: response.data });
   } else {
     res.status(404).json({ error: "Not found" });
   }
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,62 @@
 import axios from "axios";
 import Head from "next/head";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-export default function Page(props) {
+function Loading() {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="spinner">loading yagesyakk!</div>
+    </div>
+  );
+}
+function CreateImage({ prompt }) {
+  const [image, setImage] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (prompt) {
+      async function CreateImage() {
+        setLoading(true);
+        const response = await axios.post("/api/image", { prompt });
+        setImage((url) => [
+          ...url,
+          response.data.image.data[0].url,
+          response.data.image.data[1].url,
+          response.data.image.data[2].url,
+        ]);
+
+        setLoading(false);
+      }
+      CreateImage();
+    }
+  }, [prompt]);
+
+  return (
+    <div className="flex flex-row items-center gap-4">
+      {loading ? (
+        <Loading />
+      ) : (
+        image.map((img) => (
+          <div>
+            <Image
+              className="rounded-lg"
+              key={img}
+              src={img}
+              alt="Picture of the author"
+              width={256}
+              height={256}
+            />
+          </div>
+        ))
+      )}
+    </div>
+  );
+}
+
+export default function Page() {
   const [prompt, setPrompt] = useState("");
-  const [image, setImage] = useState("");
   const handleSubmit = async (event) => {
     event.preventDefault();
     const form = event.target;
@@ -16,9 +65,7 @@ export default function Page(props) {
     };
     //set prompt
     setPrompt(data.prompt);
-    //send request
-    const response = await axios.post("/api/image", data);
-    setImage(response.data.image.url);
+    //create image
   };
 
   return (
@@ -26,36 +73,30 @@ export default function Page(props) {
       <Head>
         <title>Image Generation Powered by DALL-E</title>
       </Head>
-      <div className="form-control px-auto mx-32 pt-16">
-        <p className="text-sm mb-2">Start with detail description</p>
-        <form className="input-group input-group-md" onSubmit={handleSubmit}>
-          <input
-            type="text"
-            placeholder="An Impressionist oil painting of sunflowers in a purple vase"
-            maxLength={100}
-            id="prompt"
-            className="input input-bordered w-full input-md outline-none bg-slate-200"
-            required
-          />
-          <button
-            className="btn btn-md outline-none bg-slate-200"
-            type="submit"
-          >
-            Generate
-          </button>
-        </form>
-      </div>
-      {/* if prompt is empty dont show anything */}
-      {prompt && (
-        <div className="px-auto mx-32 pt-16">
-          <Image
-            src={image}
-            alt="Generated Image"
-            width={256}
-            height={256}
-          ></Image>
+      <div className="mx-64 pt-16">
+        <div className="form-control w-full">
+          <p className="text-sm mb-2">Start with detail description</p>
+          <form className="input-group input-group-md" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="An Impressionist oil painting of sunflowers in a purple vase"
+              maxLength={100}
+              id="prompt"
+              className="input input-bordered w-full input-md outline-none bg-slate-200"
+              required
+            />
+            <button
+              className="btn btn-md outline-none bg-slate-200"
+              type="submit"
+            >
+              Generate
+            </button>
+          </form>
         </div>
-      )}
+      </div>
+      <div className="mx-64 pt-16">
+        <CreateImage prompt={prompt} />
+      </div>
     </div>
   );
 }
